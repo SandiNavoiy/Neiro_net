0d39633af83389ac1965d363d790249015726185
Sandi_Navoiy <santes.au@gmail.com>
2023-10-11 22:21:08 +0300

    рабочая версия 1

--- main.py
+++ main.py
@@ -60,3 +60,27 @@ for epoch in range(epochs):
     print(f"Точность: {round((e_correct / images.shape[0]) * 100, 3)}%")
     e_loss = 0
     e_correct = 0
+
+# случайное изображение из файла
+test_image = plt.imread("custom.jpg", format="jpeg")
+
+# Grayscale + Unit RGB + inverse colors
+gray = lambda rgb : np.dot(rgb[... , :3] , [0.299 , 0.587, 0.114])
+test_image = 1 - (gray(test_image).astype("float32") / 255)
+
+# Reshape
+test_image = np.reshape(test_image, (test_image.shape[0] * test_image.shape[1]))
+
+# Обучение
+image = np.reshape(test_image, (-1, 1))
+
+# Forward propagation (to hidden layer)
+hidden_raw = bias_input_to_hidden + weights_input_to_hidden @ image
+hidden = 1 / (1 + np.exp(-hidden_raw)) # sigmoid
+# Forward propagation (to output layer)
+output_raw = bias_hidden_to_output + weights_hidden_to_output @ hidden
+output = 1 / (1 + np.exp(-output_raw))
+
+plt.imshow(test_image.reshape(28, 28), cmap="Greys")
+plt.title(f"NN предполагает, что пользовательский номер равен: {output.argmax()}")
+plt.show()
