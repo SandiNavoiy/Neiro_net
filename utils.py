e7bfe7e51d2b2adfcbf47df738fd649518cfc9f9
Sandi_Navoiy <santes.au@gmail.com>
2023-10-11 21:16:44 +0300

    начало 2

--- utils.py
+++ utils.py
@@ -5,13 +5,15 @@ def load_dataset():
     """Функция загрузки инфы из файла"""
     with np.load("mnist.npz") as f:
         # конвертация  цветов из RGB в формат Unit RGB
-        x_train = f['x_train'].astype("float32") / 255
+        x_train = f["x_train"].astype("float32") / 255
 
         # меняем форму массива изображений из (60000, 28, 28) в (60000, 784), 784 нейрона входной нейросети 28*28 = 784
-        x_train = np.reshape(x_train, (x_train.shape[0], x_train.shape[1] * x_train.shape[2]))
+        x_train = np.reshape(
+            x_train, (x_train.shape[0], x_train.shape[1] * x_train.shape[2])
+        )
 
         # labels
-        y_train = f['y_train']
+        y_train = f["y_train"]
 
         # конвертация в двухмерный массив 60000 на 10. 10 это 10 возможных цифр
         y_train = np.eye(10)[y_train]
